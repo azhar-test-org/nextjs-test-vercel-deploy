ea61f22cf74db1d0a980e13422077472195d3a78
Mohammed Azhar Padariyakam <azharakbar@gmail.com>
2021-12-15 00:58:48 +0530

    making everything same as of main repo except .toml

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-  purge: {
-    enabled: true,
-    content: ['./pages/**/*.{js,ts,jsx,tsx}',],
-  },
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-}
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
